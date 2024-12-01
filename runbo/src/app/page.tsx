507f60772710264163ac061a80d4c15e8270d46a
aoikobayashi1 <aoi_kobayashi@sakurug.co.jp>
2024-12-01 18:33:34 +0900

    first-commit_1201

--- runbo/src/app/page.tsx
+++ runbo/src/app/page.tsx
@@ -12,6 +12,8 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <p>Hello! World!</p>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
